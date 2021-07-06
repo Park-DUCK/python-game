b5404ec28d6064b48172cfbb3ac9c4da0f11e54d
Park Deokhyeon <pdh6128@gamil.com>
2021-07-06 23:51:46 +0900

    랜덤요소 추가

--- 주사위 게임.py
+++ 주사위 게임.py
@@ -1,6 +1,11 @@
-pl_pos = 6
-com_pos = 3
+import random #랜덤 모듈 임포트
+pl_pos = 1
+com_pos = 1
 def board():
     print("·" * (pl_pos - 1) + "P" + "·" * (30-pl_pos))
     print("·" * (com_pos - 1) + "C" + "·" * (30-com_pos))
-board()
+while True:
+    board()
+    input("Enter를 누르면 말이 움직입니다") #입력대기
+    pl_pos = pl_pos + random.randint(1, 6) #난수만큼 옮김
+    com_pos = com_pos + random.randint(1, 6)
