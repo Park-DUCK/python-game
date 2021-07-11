6299f119004d515057a9953bfe6f763cf5eaacbb
Park Deokhyeon <pdh6128@gamil.com>
2021-07-11 23:27:09 +0900

    GUI 제작

--- GUI-1/list0711_0.py
+++ GUI-1/list0711_0.py
@@ -2,4 +2,5 @@ import tkinter
 root = tkinter.Tk() # 윈도우 요소(객체) 생성
 root.title("첫 번째 윈도우") #2 윈도우 제목 지정
 root.geometry("800x600") #2 윈도우 크기 지정
-root.mainloop() # 윈도우 표시 
+root.mainloop() # 윈도우 표시
+#.
